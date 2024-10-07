bdac1a63d2e6e1759ca733141413da20d4c003ec
Alexander <alexpunder@yandex.ru>
2024-10-07 18:14:24 +0300

    Initial alembic and first migration. Add User model.

--- src/database.py
+++ src/database.py
@@ -1,12 +1,7 @@
-from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from src.config import settings
-from src.constants import DB_NAMING_CONVENTION
 
 engine = create_async_engine(
     url=settings.db_settings.DSN,
-)
-metadata = MetaData(
-    naming_convention=DB_NAMING_CONVENTION,
 )
---
+++ src/users/models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from src.constants import DB_NAMING_CONVENTION
+
+
+metadata = MetaData(
+    naming_convention=DB_NAMING_CONVENTION,
+)
+
+
+class Base(DeclarativeBase):
+    metadata = metadata
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    name: Mapped[str] = mapped_column()
+    surname: Mapped[str] = mapped_column()
+    email: Mapped[str] = mapped_column()
+    phone: Mapped[str] = mapped_column()
+    is_active: Mapped[bool] = mapped_column(default=True)
+    is_blocked: Mapped[bool] = mapped_column(default=False)
+    is_superuser: Mapped[bool] = mapped_column(default=False)
